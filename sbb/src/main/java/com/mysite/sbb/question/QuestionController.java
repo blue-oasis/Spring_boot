2c08229720badd9bda8b6f607809caaf29d0e5d4
JiWon <radiohead@knou.ac.kr>
2022-06-03 15:31:54 +0900

    에러남
    QuestionController final 초기 생성자 지정하라는데 롬복에러인듯 모르겠음

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -3,12 +3,20 @@ package com.mysite.sbb.question;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+import org.springframework.ui.Model;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Controller
 public class QuestionController {
-	
-	@RequestMapping("/question/list")
-	public String list() {
-		return "question_list";
-	}
+
+    private final QuestionRepository questionRepository;
+
+    @RequestMapping("/question/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
 }
