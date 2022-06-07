04b1cfa528994f7f9407e41799314274bcc0703c
JiWon <radiohead@knou.ac.kr>
2022-06-07 17:13:38 +0900

    url prefix, 답변등록
    url prefix, 답변등록 폼 작성

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -8,21 +8,21 @@ import java.util.List;
 import org.springframework.ui.Model;
 import lombok.RequiredArgsConstructor;
 
-
+@RequestMapping("/question") //url 프리픽스, 클래스 내 메소드 url 무조건 /question 붙고 시작
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    @RequestMapping("/question/list")
+    @RequestMapping("/list")
     public String list(Model model) {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
     
-    @RequestMapping(value = "/question/detail/{id}")
+    @RequestMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
     	Question question = this.questionService.getQuestion(id);
     	model.addAttribute("question", question);
