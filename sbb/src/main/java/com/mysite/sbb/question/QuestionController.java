62e80e3975ca3deefcc80d28619d9e99ec49a51b
JiWon <radiohead@knou.ac.kr>
2022-06-07 16:45:47 +0900

    0607
    세부데이터 url 매핑, 세부 데이터 표시 템플릿, 예외처리, lombok 계속 에러나는데 재부팅하다보면 댐 ㅎ

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -2,6 +2,7 @@ package com.mysite.sbb.question;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import org.springframework.ui.Model;
@@ -19,5 +20,12 @@ public class QuestionController {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+    
+    @RequestMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+    	Question question = this.questionService.getQuestion(id);
+    	model.addAttribute("question", question);
+    	return "question_detail";
     }
 }
--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Optional;
+import com.mysite.sbb.DataNotFoundException;
+
 @RequiredArgsConstructor
 @Service //데이터 처리를 위한 서비스 만들기
 public class QuestionService {
@@ -12,5 +15,15 @@ public class QuestionService {
 	
 	public List<Question> getList() {
 		return this.questionRepository.findAll();
+	}
+	
+	public Question getQuestion(Integer id) {
+		Optional<Question> question = this.questionRepository.findById(id);
+		if (question.isPresent()) {
+			return question.get();
+		}
+		else {
+			throw new DataNotFoundException("question not found");
+		}
 	}
 }
