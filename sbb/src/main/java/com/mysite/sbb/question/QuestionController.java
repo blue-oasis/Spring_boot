842f5d79ad32efc68ec9f663267da849edf57c09
JiWon <radiohead@knou.ac.kr>
2022-06-14 11:41:41 +0900

    질문 등록
    질문 등록을 위해 서비스 create
    컨트롤러 questionCreate 생성

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -8,7 +8,11 @@ import java.util.List;
 import org.springframework.ui.Model;
 import lombok.RequiredArgsConstructor;
 
-@RequestMapping("/question") //url 프리픽스, 클래스 내 메소드 url 무조건 /question 붙고 시작
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/question") // url 프리픽스, 클래스 내 메소드 url 무조건 /question 붙고 시작
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
@@ -21,11 +25,22 @@ public class QuestionController {
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
-    
+
     @RequestMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
-    	Question question = this.questionService.getQuestion(id);
-    	model.addAttribute("question", question);
-    	return "question_detail";
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
+    }
+
+    @GetMapping("/create") // 질문 등록 get 매핑
+    public String questionCreate() {
+        return "question_form";
+    }
+
+    @PostMapping("/create") // 질문 등록 post 매핑
+    public String questionCreate(@RequestParam String subject, @RequestParam String content) {
+        this.questionService.create(subject, content); // 서비스 이용 질문 저장
+        return "redirect:/question/list"; // 저장 후 질문 목록으로 이동
     }
 }
--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.Optional;
 import com.mysite.sbb.DataNotFoundException;
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @Service //데이터 처리를 위한 서비스 만들기
@@ -25,5 +26,13 @@ public class QuestionService {
 		else {
 			throw new DataNotFoundException("question not found");
 		}
+	}
+
+	public void create(String subject, String content) { //질문 저장 서비스
+		Question q = new Question();
+		q.setSubject(subject);
+		q.setContent(content);
+		q.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q);
 	}
 }
