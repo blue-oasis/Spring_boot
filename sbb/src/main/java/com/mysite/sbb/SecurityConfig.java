bf6cc55ba2f8cb74615d867a782088de01310c64
JiWon <radiohead@knou.ac.kr>
2022-06-15 14:07:39 +0900

    로그인 기능 적용 환경설정
    spring-boot-starter-security, thymeleaf-extras-springsecurity5
    로그인 없이 조회 가능하도록 SecurityConfig 설정

---
+++ sbb/src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.mysite.sbb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+
+
+@Configuration //스프링 환경설정 파일로 지정하는 어노테이션
+@EnableWebSecurity //모든 요성 url이 스프링 시큐리티 제어 받도록
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").permitAll()
+        .and().csrf().ignoringAntMatchers("/h2-console/**")
+        .and().headers().addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
+        //로그인 안해도 모든 페이지 접근 가능하게 오버라이딩 + h2 콘솔 csrf예외
+    }
+}
