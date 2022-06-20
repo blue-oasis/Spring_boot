a4560d5afd81b0acd466b4179f2c77a83e77255f
JiWon <radiohead@knou.ac.kr>
2022-06-20 14:40:56 +0900

    회원가입 User모델
    회원가입 구현을 위해 user모델 생성

---
+++ sbb/src/main/java/com/mysite/sbb/user/SiteUser.java
@@ -0,0 +1,27 @@
+package com.mysite.sbb.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class SiteUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true) // 유일값 제약
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+}
