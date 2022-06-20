2d876868ebf62d43cf4f122c9af7d61227b5b1af
JiWon <radiohead@knou.ac.kr>
2022-06-20 14:45:03 +0900

    회원가입 비밀번호 암호화
     @Bean 등록
        public PasswordEncoder passwordEncoder() {
            return new BCryptPasswordEncoder();
        }

--- sbb/src/main/java/com/mysite/sbb/SecurityConfig.java
+++ sbb/src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+//PasswordEncoder bean 생성
 
 @Configuration //스프링 환경설정 파일로 지정하는 어노테이션
 @EnableWebSecurity //모든 요성 url이 스프링 시큐리티 제어 받도록
@@ -16,5 +20,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .and().csrf().ignoringAntMatchers("/h2-console/**")
         .and().headers().addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
         //로그인 안해도 모든 페이지 접근 가능하게 오버라이딩 + h2 콘솔 csrf예외
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
