1239e6834409d2df321e9019dc85d65b50636df6
JiWon <radiohead@knou.ac.kr>
2022-06-27 14:24:56 +0900

    검색 기능 구현
    검색 기능 구현을 위해 private Specification<Question> search(String kw 쿼리 작성
    입력 폼/자바스크립트
    페이징 버튼 링크 수정
    
    **검색은 잘 되는데 검색목록 리스트 1,2,3, 버튼누르면 검색창에 숫자가 들어가서 이상함
    ** 첫페이지/끝페이지도 검색시 고쳐야됨 잘모르겠음

--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -16,17 +16,27 @@ import org.springframework.data.domain.PageRequest;
 
 import com.mysite.sbb.user.SiteUser;
 
+import com.mysite.sbb.answer.Answer;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import org.springframework.data.jpa.domain.Specification;
+
 @RequiredArgsConstructor
 @Service //데이터 처리를 위한 서비스 만들기
 public class QuestionService {
 	
 	private final QuestionRepository questionRepository;
 
-	public Page<Question> getList(int page) { //질문 목록 조회 + 페이징 서비스 추가
+	public Page<Question> getList(int page, String kw) { //질문 목록 조회 + 페이징 서비스 추가 + 검색어 kw
         List<Sort.Order> sorts = new ArrayList<>();
 		sorts.add(Sort.Order.desc("createDate")); //작성일시 역순조건 추가 조회
 		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts)); 
-        return this.questionRepository.findAll(pageable);
+        Specification<Question> spec = search(kw);
+		return this.questionRepository.findAll(spec, pageable);
     }
 	
 	public Question getQuestion(Integer id) {
@@ -62,5 +72,24 @@ public class QuestionService {
 	public void vote(Question question, SiteUser siteUser) {
 		question.getVoter().add(siteUser);
 		this.questionRepository.save(question);
+	}
+
+	private Specification<Question> search(String kw) {
+		return new Specification<>() {
+			private static final long serialVersionUID = 1L;
+			@Override
+			public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+				query.distinct(true); //중복제거
+				Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT); // Question, SiteUser 엔티티 아우터조인(author) 이용 질문 작성자 검색
+				Join<Question, Answer> a = q.join("answerList", JoinType.LEFT); // Question, Answer 아우터조인 answerList 이용  답변 내용 검색
+				Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT); //답변 작성자 검색
+				// cb.or or조건, cb.like 검색 like
+				return cb.or(cb.like(q.get("subject"), "%" + kw + "%"), //제목
+				cb.like(q.get("content"), "%" + kw + "%"), //내용
+				cb.like(u1.get("username"), "%" + kw +"%"), //질문 작성자
+				cb.like(a.get("content"), "%" + kw +"%"), //답변 내용
+				cb.like(u2.get("username"), "%" + kw + "%")); //답변 작성자
+			}
+		};
 	}
 }
